9eb93e02c760cfc0bb81a0e32fd251fbbbb6da60
jabircode <jabirjamil@yahoo.com.sg>
2023-08-04 12:13:03 +0800

    cahnges

--- src/test/java/com/sddevops/jenkins_maven/eclipse/SongColletionTest.java
+++ src/test/java/com/sddevops/jenkins_maven/eclipse/SongColletionTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.sddevops.sonarqube_maven.eclipse.Song;
-import com.sddevops.sonarqube_maven.eclipse.SongCollection;
+import com.sddevops.jenkins_maven.eclipse.Song;
+import com.sddevops.jenkins_maven.eclipse.SongCollection;
 
 class SongColletionTest {
 	
